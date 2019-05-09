615d390179ffe01c14412497dea65a0c22f3af65
Stefan Rimaila <stefan@rimaila.fi>
2019-05-10 00:22:37 +0300

    feat(layout): Add GlobalControls component

--- src/components/layout/_/GlobalControls.js
+++ src/components/layout/_/GlobalControls.js
@@ -1,20 +1,34 @@
 import * as React from 'karet';
 import * as U from 'karet.util';
+import * as R from 'kefir.ramda';
+import * as L from 'kefir.partial.lenses';
+import { v4 as mkUuid } from 'uuid';
 
-const GlobalControls = () =>
+import * as M from '../../../meta';
+import Group from './Group';
+
+const GlobalControls = ({ store }) =>
   <section>
-    <header>
-      Global controls
-    </header>
-
-    <div>
-      <button>
+    <Group>
+      <button onClick={() => M.appendCardIn(store).set({ id: mkUuid() })}>
         Create new
       </button>
-      <button>
+      <button onClick={() => M.ghostModeIn(store).modify(R.not)}>
         Outline
       </button>
-    </div>
+    </Group>
+
+    <Group header="State">
+      <button>
+        Export
+      </button>
+      <button>
+        Import
+      </button>
+      <button>
+        Clear
+      </button>
+    </Group>
   </section>;
 
 export default GlobalControls;
