7589f093f8ac4cab0bc17b8afe2d399f469ef29a
Stefan Rimaila <stefan@rimaila.fi>
2019-05-10 12:22:56 +0300

    feat(core): Add typedefs to default data

--- src/defaults.js
+++ src/defaults.js
@@ -2,11 +2,18 @@ import { v4 as mkUuid } from 'uuid';
 
 export const minSize = [225, 225];
 
-export const storageKey = 'dora-tan:v4';
+export const storageKey = 'dora-tan:v1-2';
 
+/**
+ * @type {State}
+ */
 export const initialState = {
+  meta: {
+    storageKey,
+  },
   flags: {
     ghost: false,
+    show: [],
   },
   current: null,
   cards: [
@@ -30,3 +37,27 @@ export const initialState = {
     },
   ],
 };
+
+/**
+ * @typedef {object} State
+ * @prop {object} flags
+ * @prop {Array<String>} show
+ * @prop {Uuid} current
+ * @prop {Array<Card>} cards
+ */
+
+/**
+ * @typedef {object} Card
+ * @prop {Uuid} id
+ * @prop {[number, number]} position
+ * @prop {[number, number]} size
+ * @prop {string} content
+ */
+
+/**
+ * @typedef {object} Flags
+ */
+
+/**
+ * @typedef {string} Uuid
+ */
