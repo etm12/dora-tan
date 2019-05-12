3ac368053dea7ce5d97a7c3bfd113966e2b903bf
Stefan Rimaila <stefan@rimaila.fi>
2019-05-12 16:16:08 +0300

    feat(helpers): Add helper methods for dimensions

--- src/components/board/_/helpers.js
+++ src/components/board/_/helpers.js
@@ -1,6 +1,49 @@
-import * as K from 'kefir';
-import * as U from 'karet.util';
+import * as R from 'kefir.ramda';
+import * as I from 'infestines';
 
-export const toPx = v => `${v}px`;
+/**
+ * @template T
+ * @type {Ary1Fn<T, string>}
+ */
+export const ensureString = R.unless(R.is(String), R.toString);
+export const suffix = R.flip(R.concat);
 
-export const toCssTranslate = axis => v => `translate${axis.toUpperCase()}(${v})`;
+//
+
+export const toUnit = unit => R.pipe(ensureString, suffix(unit));
+
+export const toPx = toUnit('px');
+export const toRem = toUnit('rem');
+export const toPercent = toUnit('%');
+export const toDeg = toUnit('deg');
+
+//
+
+export const surroundWith = I.curry(function surroundWith(a, b, x) {
+  return I.seq(x, R.concat(a), R.flip(R.concat)(b));
+});
+
+export const bimap = I.curry(function bimap(f, g, xy) {
+  return [f(xy[0]), g(xy[1])];
+});
+
+// CSS ................................................................
+
+const toTranslate = R.pipe(R.toUpper, R.concat('translate'));
+
+/** @type {Ary1Fn<object, string>} */
+export const toCssTranslate = R.pipe(
+  R.toPairs,
+  R.map(R.pipe(
+    bimap(toTranslate, R.pipe(toPx, surroundWith('(', ')'))),
+    R.join(''),
+  )),
+  R.join(' '),
+);
+
+/**
+ * @callback Ary1Fn
+ * @template T0, R
+ * @param {T0} a
+ * @return {R}
+ */
